d2dce52ab61e49bf1ce676f3547ef7922d9bc968
Kumar Kartikay <krkartikay@gmail.com>
2023-03-21 20:04:13 +0530

    Initialize site with NextJS

---
+++ pages/index.tsx
@@ -0,0 +1,37 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import styles from '@/styles/Home.module.css'
+import { getSortedNotesData, Note } from '@/lib/notes';
+import Link from 'next/link';
+
+export async function getStaticProps() {
+  const allNotesData = getSortedNotesData();
+  return {
+    props: {
+      allNotesData,
+    },
+  };
+}
+
+export default function Home({ allNotesData }: { allNotesData: Note[] }) {
+  return (
+    <>
+      <h1>krkartikay&apos;s Notes</h1>
+      <section>
+        <h2>Index</h2>
+        <ul>
+          {allNotesData.map(({ id, metadata }) => (
+            <li key={id}>
+              <Link href={`/notes/${id}`}>{metadata.title}</Link>
+              <br />
+              <small>
+                {metadata.date}
+              </small>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </>
+  )
+}
