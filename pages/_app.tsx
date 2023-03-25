620d359cfc5bb5e24be9174dd9d923ef9842db3d
Kumar Kartikay <krkartikay@gmail.com>
2023-03-25 15:49:53 +0530

    fix analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,16 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Script from 'next/script'
 
 export default function App({ Component, pageProps }: AppProps) {
   return <>
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-927VSS5MXP"></script>
-    <script>
+    <Script async src="https://www.googletagmanager.com/gtag/js?id=G-927VSS5MXP"></Script>
+    <Script>
       {`window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
 
         gtag('config', 'G-927VSS5MXP');`}
-    </script>
+    </Script>
     <Component {...pageProps} /></>
 }
